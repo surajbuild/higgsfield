17727f01230e839db700d920ca76c903ef4e0f37
Suraj Kumar <surajkumar44727@gmail.com>
2026-07-24 18:59:10 +0530

    setup prisma via CLI

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,55 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.post("/api/v1/signup", (req, res) => {
+  console.log("signup called");
+  res.send("Hello sir");
+});
+
+app.get("/api/v1/signin", (req, res) => {
+  console.log("signin called");
+});
+
+app.get("/api/v1/avatar", (req, res) => {
+  console.log("avatar called");
+});
+
+app.get("/api/v1/video", (req, res) => {
+  console.log("video called");
+});
+
+app.get("/api/v1/video/:videoId", (req, res) => {
+  console.log("videoId called");
+});
+
+app.get("/api/v1/me", (req, res) => {
+  console.log("me called");
+});
+
+app.get("/api/v1/videos", (req, res) => {
+  console.log("videos called");
+});
+
+app.get("/api/v1/avatar", (req, res) => {
+  console.log("avatar called");
+});
+
+app.get("/api/v1/avatar/:avatarId", (req, res) => {
+  console.log("avatarId called");
+});
+
+app.get('/api/v1/models', (req, res) => {
+    console.log('models called');
+})
+
+app.get('/api/v1/avatars', (req, res) => {
+    console.log('avatars called');
+})
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log('App is listining on port ', PORT);
+})
