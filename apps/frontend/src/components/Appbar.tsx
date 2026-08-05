4d180722ce0ba6178121d992d7593c983e61e83b
Suraj Kumar <surajkumar44727@gmail.com>
2026-08-06 00:57:09 +0530

    chore: define port variable

--- apps/frontend/src/components/Appbar.tsx
+++ apps/frontend/src/components/Appbar.tsx
@@ -1,14 +1,18 @@
+import { useNavigate } from "react-router";
 import { Button } from "./ui/button";
 
 export function Appbar() {
+
+  const navigate = useNavigate()
   return (
     <div className="flex justify-between items-center p-4 backdrop-blur-md bg-white/10 border border-white/20 shadow-xl">
       <div>Higgsfield</div>
       <div className="flex justify-between gap-2">
-        <Button variant={"outline"} className="cursor-pointer">
+        <Button variant={"outline"} onClick={() => navigate('/signup')} className="cursor-pointer">
           Signup
         </Button>
-        <Button variant={"outline"} className="cursor-pointer">
+        <Button variant={"outline"} onClick={() => navigate('/signin')}
+        className="cursor-pointer">
           Signin
         </Button>
       </div>
