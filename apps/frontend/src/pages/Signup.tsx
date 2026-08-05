e77dc7ac260a3e0c89efd2840d4489ce8366184c
Suraj Kumar <surajkumar44727@gmail.com>
2026-08-06 01:15:42 +0530

    feat: build signup page without using react query

--- apps/frontend/src/pages/Signup.tsx
+++ apps/frontend/src/pages/Signup.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import { Card } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import axios from 'axios'
+import { BACKEND_URL } from "@/config";
+import { useNavigate } from "react-router";
 
 const Signup = () => {
-  return <div>Signup</div>;
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  async function signup() {
+    try {
+      await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        username,
+        password,
+      })
+      navigate('/signin')
+    } catch (error) {
+      alert("error while signing up")
+    }
+  }
+  return (
+    <div className="min-h-screen min-w-screen flex">
+      <div className="flex-1 min-h-screen bg-black">
+
+      </div>
+      <div className="flex-1 min-h-screen flex justify-center items-center">
+        <Card className="p-8">
+          <Input placeholder="username" onChange={(e) => setUsername(e.target.value)}>
+
+          </Input>
+
+          <Input placeholder="password" onChange={(e) => setPassword(e.target.value)}>
+
+          </Input>
+
+          <Button onClick={signup} variant={'outline'} className="bg-green-300 cursor-pointer">
+            Signup
+          </Button>
+        </Card>
+      </div>
+    </div>
+  )
 };
 
 export default Signup;
