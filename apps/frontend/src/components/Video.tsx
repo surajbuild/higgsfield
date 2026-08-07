d5b3248eb3adce8f45b1ba9d3faa8cc5512c7ab3
Suraj Kumar <surajkumar44727@gmail.com>
2026-08-07 16:56:09 +0530

    chore: style(video)

--- apps/frontend/src/components/Video.tsx
+++ apps/frontend/src/components/Video.tsx
@@ -5,7 +5,7 @@ type VideoProps = {
 export function Video({ url, title }: VideoProps) {
   return (
     <div className="p-4 m-4 rounded-xl bg-gray-100 flex justify-between flex-col w-full overflow-hidden">
-      <video src={url} className="rounded-xl" />
+      <video src={url} className="rounded-xl" autoPlay muted loop playsInline/>
       <div className="text-2xl">{title}</div>
     </div>
   );
